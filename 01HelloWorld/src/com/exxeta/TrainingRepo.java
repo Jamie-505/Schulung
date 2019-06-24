c780ed13e499b811ad3d9d4b6e4046ccfe421a1f
James Lindsly <james.lindsly@gmail.com>
2019-06-24 13:49:23 +0200

    TrainingsRepo (vor der Präsentation)

--- 01HelloWorld/src/com/exxeta/TrainingRepo.java
+++ 01HelloWorld/src/com/exxeta/TrainingRepo.java
@@ -1,4 +1,15 @@
 package com.exxeta;
 
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+@ApplicationScoped
 public class TrainingRepo {
+   public List<Training> findAll() {
+      return LongStream.range(0,100)
+              .mapToObj(i -> Training.builder().name("Training" + i).id(i).build())
+              .collect(Collectors.toList());
+   }
 }
--- 01HelloWorld/src/com/exxeta/TrainingRestService.java
+++ 01HelloWorld/src/com/exxeta/TrainingRestService.java
@@ -1,4 +1,22 @@
 package com.exxeta;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@ApplicationScoped
+@Path("/trainings")
 public class TrainingRestService {
+    @Inject
+    private TrainingRepo trainingRepo;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Training> findAllTrainings(){
+        return trainingRepo.findAll();
+    }
 }
